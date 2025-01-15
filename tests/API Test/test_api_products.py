a8cdc2a8ab4aac2e414dba5694988c5ec89f0544
Christian Nathorst <jofel.nathorst@gmail.com>
2025-01-15 16:48:23 +0800

    updates and more API tests

--- tests/API Test/test_api_products.py
+++ tests/API Test/test_api_products.py
@@ -10,3 +10,18 @@ def test_post_all_product_list():
     assert response.status_code == 200, f"Expected 200, but got {response.status_code}"
     assert response.json().get("responseCode") == 405, "Expected response code 405, but got different"
     assert response.json().get("message") == "This request method is not supported.", "Response message different than what was expected"
+
+def test_post_search_product():
+    params = {"search_product": "Tops"}
+    response = send_request("searchProduct", method="POST", params=params)
+    assert response.status_code == 200, f"Expected 200, but got {response.status_code}"
+    assert response.json().get("responseCode") == 400, "Expected response code 400, but got different"
+    assert response.json().get("message") == "Bad request, search_product parameter is missing in POST request.", \
+        "Response message different than what was expected"
+
+def test_post_search_product_without_search_product_parameter():
+    response = send_request("searchProduct", method="POST")
+    assert response.status_code == 200, f"Expected 200, but got {response.status_code}"
+    assert response.json().get("responseCode") == 400, "Expected response code 400, but got different"
+    assert response.json().get("message") == "Bad request, search_product parameter is missing in POST request.", \
+        "Response message different than what was expected"
---
+++ tests/API Test/test_api_verify_login.py
@@ -0,0 +1,3 @@
+from utils.api_helpers import send_request
+
+def test_post_to_verify_login_with_valid_detail():
