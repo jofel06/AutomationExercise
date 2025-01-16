4511c8d160446fa7d1e6f4a072f519c8d3ef597f
Christian Nathorst <jofel.nathorst@gmail.com>
2025-01-16 15:40:35 +0800

    added more API tests

--- tests/API Test/test_api_verify_login.py
+++ tests/API Test/test_api_verify_login.py
@@ -1,3 +1,16 @@
 from utils.api_helpers import send_request
 
 def test_post_to_verify_login_with_valid_detail():
+    data = {"email": "sampleuser01@yahoo.com",
+              "password": "thisispass01"}
+    response = send_request("verifyLogin", method="POST", data=data)
+    assert response.status_code == 200, f"Expected 200, but got {response.status_code}"
+    assert response.json().get("message") == "User exists!", "Response messsage different than what was expected"
+
+def test_post_to_verify_login_without_an_email_parameter():
+    data = {"password": "randompassword"}
+    response = send_request("verifyLogin", method="POST", data=data)
+    assert response.status_code == 200, f"Expected 200, but got {response.status_code}"
+    assert response.json().get("responseCode") == 400, "Expected response code 400, but got different"
+    assert response.json().get("message") == "Bad request, email or password parameter is missing in POST request.", \
+        "Response message different than what was expected"
--- utils/api_helpers.py
+++ utils/api_helpers.py
@@ -13,15 +13,15 @@ def send_request(endpoint, method="GET", headers=None, params=None, data=None):
     if method == "GET":
         response = requests.get(URL, headers=headers, params=params)
     elif method == "POST":
-        response = requests.post(URL, headers=headers, json=data) #use Json as this methods usually sends data using Json format
+        response = requests.post(URL, headers=headers, data=data) #use Json as this methods usually sends data using Json format
     elif method == "PUT":
-        response = requests.put(URL, headers=headers, json=data)
+        response = requests.put(URL, headers=headers, data=data)
     elif method == "PATCH":
-        response = requests.put(URL, headers=headers, json=data)
+        response = requests.put(URL, headers=headers, data=data)
     elif method == "DELETE":
         response = requests.delete(URL, headers=headers)
 
     if response is not None:
-        logging.info(f"Received response staus code: {response.status_code} with body: {response.json()}")
+        logging.info(f"Received response status code: {response.status_code} with body: {response.json()}")
 
     return response
